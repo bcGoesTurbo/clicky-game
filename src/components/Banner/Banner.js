0a4e379575ddcf94ac25c2098c7baacb37d6c63e
brad <bcmonster@hotmail.com>
2020-02-14 19:33:30 +0800

    updating star wars game

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './Banner.css';
 
 
-import Crawl from 'react-star-wars-crawl'
 
 // Import the necessary styles, or include them another way with your build process
 import 'react-star-wars-crawl/lib/index.css'
 
 const Banner = () => (
-    <Crawl
-      title="Episode IV"
-      subTitle="A New Hope"
-      text="It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…"
-    />
+  <div className="banner"> 
+  <div class="starwars-demo"/>
+    <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" class="star"/>
+    <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" class="wars"/>
+    <h2 class="byline" id="byline">The Clicky Game Awakens</h2>
+</div>
 )
 
 export default Banner;
--- src/images.js
+++ src/images.js
@@ -1,29 +1,29 @@
-import beth from './images/beth.png';
-import birdperson from './images/birdperson.png';
-import evilmorty from './images/evilmorty.png';
-import gianthead from './images/gianthead.png';
-import goldenford from './images/goldenford.png';
-import jerry from './images/jerry.png';
-import jessica from './images/jessica.png';
-import meeseeks from './images/meeseeks.png';
-import morty from './images/morty.png';
-import mr from './images/mr.png';
-import rick from './images/rick.png';
-import summer from './images/summer.png';
+import yoda from './images/yoda.jpeg';
+import darthVader from './images/darth-Vader.jpg';
+import lukeSkywalker from './images/lukeSkywalker.jpg';
+import hanSolo from './images/hanSolo.jpg';
+import c3po from './images/c3po.jpg';
+import princessLeia from './images/princessLeia.jpg';
+import r2d2 from './images/r2d2.jpg';
+import chewbacca from './images/chewbacca.jpg';
+import countDooku from './images/countDooku.jpg';
+import empirorPalpartine from './images/empirorPalpartine.jpg';
+import kyloRen from './images/kyloRen.jpg';
+import ray from './images/ray.jpg';
 
 const images = [
-  beth,
-  birdperson,
-  evilmorty,
-  gianthead,
-  goldenford,
-  jerry,
-  jessica,
-  meeseeks,
-  morty,
-  mr,
-  rick,
-  summer
+  yoda,
+  darthVader,
+  lukeSkywalker,
+  hanSolo,
+  c3po,
+  princessLeia,
+  r2d2,
+  chewbacca,
+  countDooku,
+  empirorPalpartine,
+  kyloRen,
+  ray
 ];
 
 export default images;
