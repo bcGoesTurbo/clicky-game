bb93c84efc80aa2d2eeeb79dc42424f9b80a9f6e
brad <bcmonster@hotmail.com>
2020-02-15 10:06:22 +0800

    make it better

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './Banner.css';
 
-
-
-// Import the necessary styles, or include them another way with your build process
-import 'react-star-wars-crawl/lib/index.css'
-
 const Banner = () => (
-  <div className="banner"> 
-  <div class="starwars-demo"/>
-    <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" class="star"/>
-    <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" class="wars"/>
-    <h2 class="byline" id="byline">The Clicky Game Awakens</h2>
-</div>
-)
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
+  </div>
+);
 
 export default Banner;
+
+
+
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 const Navbar = props => (
   <div className="navbar ">
-    <div>Clicky Game</div>
+    <div>Star Wars Clicky Game</div>
     <div className={props.navMsgColor}>{props.navMessage}</div>
     <div>
       Score: {props.score} <span className="pipe">|</span> High Score: {props.highScore}
