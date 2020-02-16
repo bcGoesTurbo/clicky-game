4805ab2c3fcdab3a4d6594839a6983e2db828b69
brad <bcmonster@hotmail.com>
2020-02-16 10:22:53 +0800

    updated readme file

---
+++ src/README.md
@@ -0,0 +1,10 @@
+# Sports Scraper!
+
+View it live at: <https://sports-scraper-app.herokuapp.com/>
+
+### Overview
+
+This is web app that lets users "scrape" articles from Fox Sports website, save the ones they link, and allows them to leave comments on the articles.
+
+This app uses MongoDB and is deployed on Heroku.
+
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,7 @@ import CWlogo from '../../images/CWlogo.png'
 const Banner = () => (
   <div className="banner text-center d-flex align-items-center justify-content-center">
     <img src={CWlogo} alt="Logo" />
-    {/* <p className="text">Click on an image to earn points, but don't click on any more than once!</p> */}
+    {/* <p className="text"></p> */}
   </div>
 )
 
